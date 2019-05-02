b2c2d3bd30c8f975d0e48b8096526498c972ab18
Spenteliwi <47823346+Spenteliwi@users.noreply.github.com>
2019-05-02 15:27:48 +0200

    Add files via upload

--- equivalence plot function.R
+++ equivalence plot function.R
@@ -1,6 +1,5 @@
 TOST_binom_plot<-function(x,n,Ref,Loweq,Higheq,conf.level){
      require(ggplot2)
-  require(pwr)
   
   CI.Low<-matrix(binom.test(x,n,Loweq,conf.level = conf.level,alternative = "greater")$conf.int,nrow = 1)[1,1]
   CI.High<-matrix(binom.test(x,n,Higheq,conf.level = conf.level,alternative = "less")$conf.int,nrow = 1)[1,2]
@@ -12,11 +11,10 @@ TOST_binom_plot<-function(x,n,Ref,Loweq,Higheq,conf.level){
   data<-data.frame(Test,CI.Low,CI.High,p.Low,p.High)
   data$stat.diff.p_value<-Stat.diff
   
-###############PLOTING TEST##############################################################################
   plot<-ggplot()+
   
   geom_vline(xintercept = Ref,linetype = "dashed",col=3,lwd=1)+#ref value
-  geom_text(aes(x = (Ref-(Ref/50)),y=0.55,label="Ref value"),col=3,size=4)+
+  geom_text(aes(x = (Ref-(Ref/50)),y=0.55,label="Equivalent devices"),col=3,size=4)+
   
   geom_vline(xintercept = Higheq,linetype = "dashed",lwd=1)+ #Uppereq 
   geom_vline(xintercept = Loweq,linetype = "dashed",lwd=1)+ #Lowereq
@@ -24,9 +22,9 @@ TOST_binom_plot<-function(x,n,Ref,Loweq,Higheq,conf.level){
   geom_point(aes(x=Test,y=0.5),shape=15,size=7, col=1)+
   geom_segment(aes(x = Test, y = 0.5, xend = CI.Low, yend = 0.5), col=1,lwd=2)+ #IClower
   geom_segment(aes(x = Test, y = 0.5, xend = CI.High, yend = 0.5), col=1,lwd=2)+ #ICupper
-  geom_text(aes(x = Test,y=0.51,label="Test value"),col=1,size=4)+
+  geom_text(aes(x = Test,y=0.51,label="OS2-V"),col=1,size=4)+
   
-  xlab(label = "Rate")+
+  xlab(label = "Union rate")+
   ylim(0.4,0.6)+
   
   theme_classic()+
@@ -35,23 +33,12 @@ TOST_binom_plot<-function(x,n,Ref,Loweq,Higheq,conf.level){
         axis.ticks.y=element_blank())+
     scale_x_continuous(labels = scales::percent)
    
-###############Equivalence testing##############################################################################
+
 if (CI.Low<=Loweq){data$Result<- "inconclusive"}
   else{if (CI.Low>Ref){data$Result<- "Superior"}
     else {if (CI.Low>Loweq & CI.High<Higheq){data$Result<- "Equivalent"} 
       else  {if (CI.Low>Loweq & CI.High>=Higheq){data$Result<- "Not_inferior"}}}}
 
-###############Power Caluclation################################################################################    
-PL<-pwr.p.test(h=ES.h(p1 = Test,p2= Loweq),n=n, sig.level = conf.level,alternative = "greater")$power
-PH<-pwr.p.test(h=ES.h(p1 = Test,p2= Higheq),n=n, sig.level = conf.level,alternative = "less")$power
-Psup<-pwr.p.test(h=ES.h(p1 = Test,p2= Ref),n=n, sig.level = conf.level,alternative = "greater")$power
-P<-min(PL,PH)
-
-if (data$Result=="Superior"){data$Power<-Psup}
-else{if (data$Result=="Not_inferior"){data$Power<-PL}
-  else {if (data$Result=="Equivalent"){data$Power<-P} 
-    else  {data$Power<-NA}}}
-###############OUTPUTS################################################################################      
   print(data)
   plot
 }
