bdb34dd52fe9fdedf3e143027e71b5e3a76a0299
Spenteliwi <47823346+Spenteliwi@users.noreply.github.com>
2019-09-30 14:34:54 +0200

    Add files via upload

--- Binomial conf simulation.R
+++ Binomial conf simulation.R
@@ -1,29 +1,28 @@
-dat <- NULL
-real.Rate <- 0.5
-B <- rbinom(10000, 1, real.Rate)
-n = 50
+require(ggplot2)
+require(ggthemes)
+
+dat <- NULL #dataframe
+real.Rate <- 0.95
+B <- rbinom(10000, 1, real.Rate) #binomial distribution with 0 or 1 possibilities on 10000 observation
+n = 30 #sample size
 conf.level = 0.95
 
-
+#looping
 for (i in 1:100) {
     S <- sample(B, n)
     x <- length(S[S == TRUE])
     Rate <- x / n
-    CIL <- binom.confint(x, n, conf.level, methods = "wilson")[, 5]
-    CIH <- binom.confint(x, n, conf.level, methods = "wilson")[, 6]
-    Inclu <- real.Rate >= CIL & real.Rate <= CIH
+    CIL <- binom.confint(x, n, conf.level, methods = "wilson")[, 5] #Lower confidence interval
+    CIH <- binom.confint(x, n, conf.level, methods = "wilson")[, 6] #Upper confidence interval
+    Inclu <- real.Rate >= CIL & real.Rate <= CIH # testing the inclusion of the real rate within the CI
     df <- data.frame(Rate, Inclu, CIL, CIH)
     dat <- rbind(dat, df)
 }
-
-
-dat
-require(ggplot2)
-require(ggthemes)
-
+#plot
 ggplot(dat, aes(x = 1:100, y = Rate, col = Inclu)) +
     geom_point(size = 1) +
-    geom_errorbar(aes(ymax = CIH, ymin = CIL, alpha = 0.03)) +
+    geom_errorbar(aes(ymax = CIH, ymin = CIL)) +
     geom_hline(yintercept = real.Rate) +
     theme_pander() +
     scale_colour_brewer(palette = "Set1")
+
